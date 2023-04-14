fba8f5c354632c8838bfc79127fdd8d3cca59ee7
wonder202 <leehyesu95@gmail.com>
2023-04-14 18:15:00 +0900

    20230414

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert("hi");
+//const와 let의 차이 설명 및 간단한 계산기
+// const a = 9;
+// const b = 8;
+// let myName = "bennie";
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log("hello " + myName);
+
+// myName = "benedict";
+
+// console.log("Your full name is " + myName);
+
+// const amIFat  = false;
+// const amISlim = null;
+// let something;
+// console.log(something, amIFat);
+
