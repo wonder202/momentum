53df7024b8791ce7e98e274c7e6a150aca2164d4
wonder202 <leehyesu95@gmail.com>
2023-04-28 19:00:56 +0900

    20230428

--- app.js
+++ app.js
@@ -415,20 +415,18 @@ const loginInput = document.querySelector("#login-form input");
 const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden" //class가 아닌 string자체를 가져올뿐인가..?
+const HIDDEN_CLASSNAME = "hidden" //class안의 태그가 아닌 string자체를 가져올뿐
 
 //로그인 정보 전송 시
 function handleLoginSubmit(event){ //event object정보 공간 할당을 위해 (event)를 넣어줌.
     event.preventDefault(); //submit 후 자동으로 새로고침 되는 걸 해제.
-    const username = loginInput.value
+    const username = loginInput.value;
     // console.log(event); //발생한 event에 대한 정보(object형태)를 가지고 있음.
-    // console.log(loginInput.value);
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    console.log(username);
     // greeting.innerText = "Hello, " + username;
     //위와 같은 결과, 다른 함수
-    greeting.innerText = `Hello, ${username}`
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+    greeting.innerText = `Hello, ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
