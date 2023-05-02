d79e80e646334778739b74e9258f84f3bc2839d3
wonder202 <leehyesu95@gmail.com>
2023-05-02 19:22:49 +0900

    20230502

--- app.js
+++ app.js
@@ -415,27 +415,50 @@ const loginInput = document.querySelector("#login-form input");
 const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden" //class안의 태그가 아닌 string자체를 가져올뿐
+//반복해서 입력하는 string은 오타가 날 수 있으므로, 변수로 저장해준다.
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
 //로그인 정보 전송 시
 function handleLoginSubmit(event){ //event object정보 공간 할당을 위해 (event)를 넣어줌.
     event.preventDefault(); //submit 후 자동으로 새로고침 되는 걸 해제.
-    const username = loginInput.value;
-    // console.log(event); //발생한 event에 대한 정보(object형태)를 가지고 있음.
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    // greeting.innerText = "Hello, " + username;
-    //위와 같은 결과, 다른 함수
-    greeting.innerText = `Hello, ${username}`;
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);//localStorage에 저장시켜주는 식.
+    // localStorage.setItem(USERNAME_KEY, loginInput.value);
+    // console.log(event); //발생한 event에 대한 정보(object형태)를 가지고 있음.
+    
+    paintGreetings(username);
+    // paintGreetings();
+}
+// //링크달기
+// function handleLinkClick(event){
+//     event.preventDefault(); //link click시 자동으로 이동되는 걸 해제.
+//     console.dir(event); //alert보다 위에 와야 정보가 뜸!
+// }
+
+// function paintGreetings(){
+function paintGreetings(myName){
+    // const myName = localStorage.getItem(USERNAME_KEY); //new
+    // greeting.innerText = "Hello, " + myName;
+    // 위와 같은 결과, 다른 함수
+    greeting.innerText = `Hello, ${myName}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+// //function을 호출할때, 자동으로 event object를 첫번째 argument(실제값)으로 넘겨준다.
+// loginForm.addEventListener("submit", handleLoginSubmit);
 
-
-function handleLinkClick(event){
-    event.preventDefault(); //link click시 자동으로 이동되는 걸 해제.
-    console.dir(event); //alert보다 위에 와야 정보가 뜸!
+// //userName을 localStorage에 저장.
+const savedUsername = localStorage.getItem(USERNAME_KEY); //저장된 변수 불러오기.
+if(savedUsername === null) {
+    //show the loginForm
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", handleLoginSubmit);
 }
-
-//function을 호출할때, 자동으로 event object를 첫번째 argument(실제값)으로 넘겨준다.
-loginForm.addEventListener("submit", handleLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+else {
+    //show the greetings
+    paintGreetings(savedUsername);
+    // paintGreetings();
+}
+// link.addEventListener("click", handleLinkClick);
