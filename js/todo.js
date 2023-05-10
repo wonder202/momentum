380962809b5398f03e20f4d107c16abcd5c5dd9d
wonder202 <leehyesu95@gmail.com>
2023-05-10 20:54:16 +0900

    20230510

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,12 @@
 const todoForm = document.getElementById("todo-form"); //아래와 같음
 const todoList = document.querySelector("#todo-list"); //위와 같음
 const todoInput = todoForm.querySelector("input");
+const TODOS_KEY = "todos"
+let todos = []; //변경 가능한 변수.
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos)); //문자열로 저장.
+}
 
 function deleteToDo(event){
     const deleteOne = event.target.parentElement; //클릭이벤트하면 부모요소를 가진 목록 중 하나를 타겟팅
@@ -8,6 +14,7 @@ function deleteToDo(event){
     // console.dir(event.target.parentElement.innerText);//어떤 버튼인지 text를 타겟팅해서 출력
 }
 
+//화면에 그려주기
 function createToDo(todo){
     const li = document.createElement("li");
     const span = document.createElement("span");
@@ -26,6 +33,30 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = todoInput.value; //변수값을 복사
     todoInput.value = "";
-    createToDo(newToDo);
+    todos.push(newToDo); //newToDo를 배열로 만들어주기
+    createToDo(newToDo); //newToDo를 화면에 그려주기
+    saveToDos(); //저장하기
 }
 todoForm.addEventListener("submit", handleToDoSubmit);
+
+// function sayHello(items){ //배열안, 각각의 item에 대한 정보가 하나씩 자동으로 들어감.
+//     console.log("this is", items);
+//     // 콘솔창 ex)
+//     // this is sds
+//     // this is d
+//     // this is sd
+// }
+
+//새로고침 시 저장된 todos 불러오기.
+const savedToDos = localStorage.getItem(TODOS_KEY); 
+// if(savedToDos !== null){
+if(savedToDos){ //localStorage에 목록이 있을 경우.
+    const parsedToDos = JSON.parse(savedToDos); //문자열로 저장시킨 것을 다시 배열로 변환하여 가져오기.
+    todos = parsedToDos;
+    // parsedToDos.forEach(sayHello); //parsedToDos 배열안의 item 각각에게 function 적용.
+    // 위에 만들어준 sayHello와 같은 function 구문을 따로 만들 필요없이 아래와 같이 작성가능. 같은 것임.
+    // arrow function이라고 함.
+    // parsedToDos.forEach((items) => console.log("this is", items));
+
+    parsedToDos.forEach(createToDo);
+}
