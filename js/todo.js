277b15aad96c257d3f72eea44c08a0ae0b381a29
wonder202 <leehyesu95@gmail.com>
2023-05-11 21:49:52 +0900

    20230511

--- js/todo.js
+++ js/todo.js
@@ -2,7 +2,7 @@ const todoForm = document.getElementById("todo-form"); //아래와 같음
 const todoList = document.querySelector("#todo-list"); //위와 같음
 const todoInput = todoForm.querySelector("input");
 const TODOS_KEY = "todos"
-let todos = []; //변경 가능한 변수.
+let todos = []; //변경 가능한 변수.최종 array
 
 function saveToDos(){
     localStorage.setItem(TODOS_KEY, JSON.stringify(todos)); //문자열로 저장.
@@ -10,15 +10,21 @@ function saveToDos(){
 
 function deleteToDo(event){
     const deleteOne = event.target.parentElement; //클릭이벤트하면 부모요소를 가진 목록 중 하나를 타겟팅
-    deleteOne.remove(); //선택한 목록 삭제
+    deleteOne.remove(); //클릭한 목록 치우기. db엔 정보가 남아있음.
+
+    //여기서 db에 남아있던 arr정보 중, (삭제)빼고자하는 id를 가진 item을 제외하고 새로운 arr을 만듬. 
+    //즉, 덧씌우는 개념.
+    todos = todos.filter(savedtodo => savedtodo.id !== parseInt(deleteOne.id));
+    saveToDos(); //새로운 arr을 저장시켜줌.
     // console.dir(event.target.parentElement.innerText);//어떤 버튼인지 text를 타겟팅해서 출력
 }
 
 //화면에 그려주기
 function createToDo(todo){
     const li = document.createElement("li");
+    li.id = todo.id;
     const span = document.createElement("span");
-    span.innerText = todo;
+    span.innerText = todo.text; //obj전체가 아닌 text만 그려지도록함.
 
     const button = document.createElement("button");
     button.innerText = "❌";
@@ -33,8 +39,14 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = todoInput.value; //변수값을 복사
     todoInput.value = "";
-    todos.push(newToDo); //newToDo를 todos변수에 저장.
-    createToDo(newToDo); //newToDo를 화면에 그려주기.
+
+    const newToDoObj = {
+        text : newToDo,
+        id   : Date.now(),
+    } //item을 특정하기위해 object형식으로 id를 부여.
+
+    todos.push(newToDoObj); //newToDoObj를 todos변수에 저장.
+    createToDo(newToDoObj); //newToDoObj를 화면에 그려주기.
     saveToDos(); //저장하기
 }
 todoForm.addEventListener("submit", handleToDoSubmit);
